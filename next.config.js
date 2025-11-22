0741bc425583e03da754f957fd5f4844e017a0f5
minh hieu <minhhieudev@gmail.com>
2025-11-22 22:06:47 +0700

    fix: Add ESLint config to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: false, // Set to true if you want to ignore ESLint errors during build
+  },
   experimental: {
     optimizePackageImports: [],
     scrollRestoration: false,         // Disable scroll restoration for manual control
